9befdaf7e1201f85bb10622c17773248047a4149
Cristóvão Antunes <cristovao.av.antunes@gmail.com>
2026-05-16 17:03:22 +0100

    add base shadcn configs

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,13 @@ import {
   SignUpButton,
   UserButton,
 } from "@clerk/nextjs";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geistMonoHeading = Geist_Mono({subsets:['latin'],variable:'--font-heading'});
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +35,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", inter.variable, geistMonoHeading.variable)}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
